b0853236e0824b5013ea9b2e98f8d002fd97eb09
Habboista <antonio.fusillo99@gmail.com>
2024-02-20 14:15:53 +0100

    Computation on computer, arduino code lighter

--- arduino/servo/servo.ino
+++ arduino/servo/servo.ino
@@ -1,15 +1,10 @@
 #include <Servo.h>
 
-#define BUFFLEN 10;
-
 Servo servoPan;  // Create a servo object
 Servo servoTilt;
 
-float mapFloat(float value, float fromLow, float fromHigh, float toLow, float toHigh) {
-  return (value - fromLow) * (toHigh - toLow) / (fromHigh - fromLow) + toLow;
-}
-int xmax;
-int ymax;
+int pan;
+int tilt;
 
 void setup() {
   Serial.begin(9600);  // Initialize serial communication at 9600 bps
@@ -22,26 +17,10 @@ void setup() {
 void loop() {
   // Check if there's data available to read
   if (Serial.available() > 0) {
-    // Read x and y coordinates from serial
-    float targetX = Serial.parseFloat();
-    float targetY = Serial.parseFloat();
-    float targetZ = Serial.parseFloat();
-    
-    xmax = 1920*targetZ*25/(24*1080);
-    ymax = 1080*targetZ*25/(24*1080);
+    pan = Serial.parseInt();
+    tilt = Serial.parseInt();
 
-    targetX += xmax/2;
-    targetY += ymax/2;
-    targetX /= max(xmax, 1e-5);
-    targetY /= max(ymax, 1e-5);
-
-    // Smoothly move the servos to the target position
-    if (targetX > 0 && targetY > 0 && targetX < 1 && targetY < 1) {
-      int pan = int(mapFloat(targetX, 0, 1, 0, 180));
-      int tilt = int(mapFloat(targetY, 0, 1, 0, 180));
-
-      servoPan.write(pan);
-      servoTilt.write(tilt);
-    }
+    servoPan.write(pan);
+    servoTilt.write(tilt);
   }
 }
--- src/Arduino.py
+++ src/Arduino.py
@@ -1,4 +1,5 @@
 import serial
+import numpy as np
 
 # Establish serial communication
 
@@ -7,20 +8,35 @@ class Arduino:
     def __init__(self, arduino_port="/dev/cu.usbmodem21301"):
         self.ser = serial.Serial(arduino_port, 9600, timeout=1)
 
+    def set_computer_position(self, arduino_computer: np.ndarray):
+        """Set computer position with respect to arduino reference system"""
+        self.arduino_computer = arduino_computer
+
     def send_coordinates(self, x, y, z, IMG_SIZE=(1920, 1080), scaling_factor=1.0):
-        """Send x,y,z coordinates in centimeters through serial communication"""
+        """Send pan and tilt angles in degrees through serial communication"""
         dst_size = IMG_SIZE[0] * scaling_factor, IMG_SIZE[1] * scaling_factor
         x /= dst_size[0]
         y /= dst_size[1]
+
         x_max = 1920*z*25/(24*1080)
         y_max = 1080*z*25/(24*1080)
-        #x = round(x, 3)
-        #y = round(y, 3)
+        
         x *= x_max
         y *= y_max
+
         x -= x_max/2
         y -= y_max/2
-        data = f"{x},{y},{z}\n"
+
+        computer_target = np.array([x, y, z])
+        arduino_target = self.arduino_computer + computer_target
+
+        pan = np.arccos(-arduino_target[0] / np.sqrt(arduino_target[0]**2 + arduino_target[2]**2))
+        tilt = np.arccos(-arduino_target[1] / np.sqrt(arduino_target[1]**2 + arduino_target[2]**2))
+
+        pan = np.rad2deg(pan)
+        tilt = np.rad2deg(tilt)
+        
+        data = f"{int(pan)},{int(tilt)}\n"
         self.ser.write(data.encode("utf-8"))
 
     def close(self):
