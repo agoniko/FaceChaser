384ac49482b7e3585cfba7c3bbdf0cc7dc3f0ae2
Habboista <antonio.fusillo99@gmail.com>
2024-02-23 00:59:22 +0100

    virtual arduino through port "virtual"

--- src/Arduino.py
+++ src/Arduino.py
@@ -9,7 +9,10 @@ from src.refsys.vector import Vector
 
 class Arduino:
     def __init__(self, serial_port, ref_sys_list: List[ReferenceSystem]):
-        self.ser = serial.Serial(serial_port, 9600, timeout=1)
+        if serial_port != "virtual":
+            self.ser = serial.Serial(serial_port, 9600, timeout=1)
+        else:
+            self.virtual = True
         self.ref_sys_list = ref_sys_list
 
     def set_computer_position(self, arduino_computer: np.ndarray):
@@ -19,6 +22,7 @@ class Arduino:
     def send_coordinates(self, target: Vector):
         """Send pan and tilt angles in degrees through serial communication"""
         target.to(self.ref_sys_list[-1])
+        target.detach()
 
         pan = np.arccos(-target.array[0] / np.sqrt(target.array[0]**2 + target.array[2]**2))
         tilt = np.arccos(-target.array[1] / np.sqrt(target.array[1]**2 + target.array[2]**2))
@@ -27,7 +31,9 @@ class Arduino:
         tilt = np.rad2deg(tilt)
         
         data = f"{int(pan)},{int(tilt)}\n"
-        self.ser.write(data.encode("utf-8"))
+        if not self.virtual:
+            self.ser.write(data.encode("utf-8"))
 
     def close(self):
-        self.ser.close()
+        if not self.virtual:
+            self.ser.close()
